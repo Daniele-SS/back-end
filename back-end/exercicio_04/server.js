f5577d6084834e0134474a17816085546d676d4e
Daniele-SS <dev.daniele@outlook.com>
2026-02-20 10:08:31 -0300

    Novo exercicio iniciado

---
+++ back-end/exercicio_04/server.js
@@ -0,0 +1,6 @@
+/**************************************************************************************************
+ * Objetivo: Arquivo responsável pelas funções de calcular (SOMAR, SUBTRAIR, MULTIPLICAR E DIVIDIR)
+ * Data: 20/02/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ **************************************************************************************************/
