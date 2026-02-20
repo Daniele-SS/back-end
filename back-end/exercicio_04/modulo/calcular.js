cab208d7eb65b745a876596c62d5776be88cef0c
Daniele-SS <dev.daniele@outlook.com>
2026-02-20 11:27:57 -0300

    Exercicio 04 finalizado

--- back-end/exercicio_04/modulo/calcular.js
+++ back-end/exercicio_04/modulo/calcular.js
@@ -12,22 +12,44 @@
 /*Modelo de função anonima
 Objetivo da função: Calcular as 4 operações matemáticas*/
 const calcular = function(numero1, numero2, operador){
+    //Entradas da função
     let valor1 = Number (numero1)
     let valor2 = Number(numero2)
+    resultado = false
     let operadorMatematico = String(operador).toUpperCase()
 
-    //Condicionais para validar qual o tipo de operação matemática
-    if(operadorMatematico == 'SOMAR'){
-        resultado = valor1 + valor2
-    }else if(operadorMatematico == 'SUBTRAIR'){
-        resultado = valor1 - valor2
-    }else if(operadorMatematico == 'MULTIPLICAR'){
-        resultado = valor1 * valor2
-    }else if(operadorMatematico == 'DIVIDIR'){
-        resultado = valor1 / valor2
+    //Condicionais para validar qual é o tipo de operação matemática
+    /**A ausencia da {} na condicional é porque qualquer condicional que tenha
+     * apenas uma linha de processamento a {} torna-se opcional*/
+    // if(operadorMatematico == 'SOMAR')
+    //     resultado = valor1 + valor2
+    // else if(operadorMatematico == 'SUBTRAIR')
+    //     resultado = valor1 - valor2
+    // else if(operadorMatematico == 'MULTIPLICAR')
+    //     resultado = valor1 * valor2
+    // else if(operadorMatematico == 'DIVIDIR')
+    //     resultado = valor1 / valor2
+   
+
+    switch (operadorMatematico) {
+        case 'SOMAR': //if
+            resultado = valor1 + valor2
+            break;
+    
+        case 'SUBTRAIR': //esle if
+            resultado = valor1 - valor2
+            break;
+
+        case 'MULTIPLICAR': //else if
+            resultado = valor1 * valor2
+            break;
+
+        case 'DIVIDIR': //else if
+            resultado = valor1 / valor2
+            break;
     }
 
-    return Number(resultado).toFixed(2)
+    return resultado
 }
 
-console.log(calcular(10, 20, 'DIVIDIR'))
+console.log(calcular(100, 50, 'multiplicar'))
