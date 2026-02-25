efac61f116d50119c117a9d64d1d574987947566
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2026-02-25 11:26:30 -0300

    Novos arquivos adicionados

---
+++ back-end/exercicio_05/app.js
@@ -0,0 +1,14 @@
+/****************************************************************************
+ * Objetivos: 
+ * 1. Criar um sistema que faça o cálculo do IMC de uma pessoa.
+ * 2. Criar um sistema que gerencie as médias escolares de uma universidade.
+ * 3. Criar um sistema para gerenciar o cálculo de uma tabuada.
+ * 4. Calcular o Fatorial de um número fornecido pelo usuário.
+ * 5. Criar um sistema que gerencie números pares e ímpares
+ * Data: 25/02/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ ****************************************************************************/
+
+
+
