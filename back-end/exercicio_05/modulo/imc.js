47a0d431300268b475e884773234083c3ffbb143
Daniele-SS <dev.daniele@outlook.com>
2026-02-27 10:21:05 -0300

    Calculo do IMC finalizado!

--- back-end/exercicio_05/modulo/imc.js
+++ back-end/exercicio_05/modulo/imc.js
@@ -1,16 +1,38 @@
-/****************************************************************************
- * Objetivos: Criar um sistema que faça o cálculo do IMC de uma pessoa.
+/*********************************************************************************************
+ * Objetivos: Arquivo resposável por criar um sistema que faça o cálculo do IMC de uma pessoa.
  * Data: 27/02/2026
  * Autora: Daniele Silva Santos
  * Versão: 1.0
- ****************************************************************************/
+ *********************************************************************************************/
 
 function calcularImcDoUsuario(altura, peso){
-    let alturaDoUsuario = altura
-    let pesoDoUsuario = peso
+    let alturaDoUsuario = 1.66
+    let pesoDoUsuario   = 90
+    const calculo = Number(pesoDoUsuario) / Number(alturaDoUsuario * alturaDoUsuario)
 
-    Number(altura * altura) / Number(pesoDoUsuario)
+    if (calculo < 18.5) {
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está abaixo do peso!`)
+
+    }else if (calculo >= 18.5 && calculo <= 24.9) {
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está com o peso normal.`)
+
+    }else if(calculo >= 25 && calculo <= 29.9){
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está acima do peso (sobrepeso).`)
+
+    }else if (calculo >= 30 && calculo <= 34.9) {
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está com Obesidade I!`)
+
+    }else if (calculo >= 35 && calculo <= 39.9) {
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está com Obesidade II!`)
+
+    }else if (calculo >= 40) {
+         console.log(`Seu peso é ${calculo.toFixed(2)}, você está com Obesidade III!`)
+    }else{
+        console.log('merda')
+    }
 }
+
+calcularImcDoUsuario()
 
 module.exports = {
     calcularImcDoUsuario
