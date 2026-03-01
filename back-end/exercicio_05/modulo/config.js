afcba1f032a4e4bf63a61cfd75d51b3359d8cf0c
Daniele-SS <daniiellesantos100@gmail.com>
2026-03-01 17:55:05 -0300

    Comentarios adicionados

--- back-end/exercicio_05/modulo/config.js
+++ back-end/exercicio_05/modulo/config.js
@@ -1,6 +1,8 @@
-// modulo/config.js
+//Importa o módulo nativo do Node para leitura do terminal
 const readline = require('readline')
 
+//Função que transforma o question em uma Promessa (Promise)
+//Isso permite usar o 'await' no app.js, deixando o código reto e limpo
 const entradaDados = (pergunta) => {
     const rl = readline.createInterface({
         input: process.stdin,
@@ -9,8 +11,8 @@ const entradaDados = (pergunta) => {
 
     return new Promise((resolve) => {
         rl.question(pergunta, (resposta) => {
-            rl.close()
-            resolve(resposta)
+            rl.close()//Fecha a interface após a resposta
+            resolve(resposta)//Retorna o que o usuário digitou
         })
     })
 }
