28b022e4ba6b3df02bc81700a3a59be11962c78c
Daniele-SS <daniiellesantos100@gmail.com>
2026-03-01 16:02:36 -0300

    chore: limpeza do projeto para reinício com arquitetura modular e assíncrona

--- back-end/exercicio_05/modulo/mediaEscolar.js
+++ back-end/exercicio_05/modulo/mediaEscolar.js
@@ -5,26 +5,6 @@
  * Versão: 1.0
  **********************************************************************************************************/
 
-function mediaEscolar(nota1, nota2, nota3, nota4, notaDoExame){
-    let primeiraNota    = nota1 
-    let segundaNota     = nota2
-    let terceiraNota    = nota3
-    let quartaNota      = nota4
-    let exame           = notaDoExame
-    let mediaDoAluno = media
-
-    if (mediaDoAluno > 50) {
-        console.log('Você está reprovado!')
-
-    }else if (mediaDoAluno <= 50 && mediaDoAluno >= 69) {
-        console.log('Você está de exame')
-    }
-
-
-
-
-
-
-
-
+function mediaEscolar(nota1, nota2, nota3, nota4,){
+ 
 }
