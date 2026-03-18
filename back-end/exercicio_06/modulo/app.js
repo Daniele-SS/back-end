2a09c9e222b374c9e956621f5782179a29118f50
Daniele-SS <dev.daniele@outlook.com>
2026-03-18 11:19:47 -0300

    Iniciando novo exercicio

--- back-end/exercicio_06/modulo/app.js
+++ back-end/exercicio_06/modulo/app.js
@@ -1,3 +1,12 @@
+/****************************************
+ * Objetivo: Obter uma lista de estados
+ * Autora: Daniele Silva Santos
+ * Data: 18/03/2026
+ * Versão: 1.0
+ ****************************************/
+
+
+
 module.export = {
     listaDeEstados
 }
