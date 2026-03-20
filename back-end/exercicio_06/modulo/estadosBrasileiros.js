c286f5276f427eed5c85879f098634d95497279a
Daniele-SS <dev.daniele@outlook.com>
2026-03-20 11:24:35 -0300

    Função getCapitalPais iniciada...

--- back-end/exercicio_06/modulo/estadosBrasileiros.js
+++ back-end/exercicio_06/modulo/estadosBrasileiros.js
@@ -5,6 +5,7 @@
  * Versão: 1.0
  ****************************************/
 
+const { describe } = require('node:test')
 const listaDeEstados = require('./estados_cidades.js')
 
 const getListaDeEstados = function () {
@@ -25,13 +26,13 @@ const getDadosEstado = function (uf) {
 
     listaDeEstados.estados.forEach(function(descricao) {
         if (descricao.sigla === uf) {
-            resultado = {
-                uf: descricao.sigla,
-                descricao: descricao.nome,
-                capital: descricao.capital,
-                regiao: descricao.regiao
+                resultado = {
+                    uf: descricao.sigla,
+                    descricao: descricao.nome,
+                    capital: descricao.capital,
+                    regiao: descricao.regiao
 
-            }//Fechamento do JSON
+                }//Fechamento do JSON
 
         }//Fechamento do if
 
@@ -44,7 +45,7 @@ const getCapitalEstado = function (uf) {
 
     listaDeEstados.estados.forEach(function(result) {
         if (result.sigla === uf) {
-            info = {
+                info = {
                 uf: result.sigla,
                 descricao: result.nome,
                 capital: result.capital
@@ -55,22 +56,43 @@ const getCapitalEstado = function (uf) {
 }
 
 const getEstadosRegiao = function (reg) {
-    let aux = []//A variável vira um array porque uma região tem vários estados
+    let aux = []/*A variável vira um array porque uma região tem vários estados. 
+                Será preciso percorrer o array para localizar a região e seus estados */
 
     listaDeEstados.estados.forEach(function(estado) {
         if (estado.regiao === reg) {
-            /**O .push() é usado para poder 'puxar' todos os estados
-                da região selecionada */
-            aux.push({
-                regiao: estado.regiao,
+            aux.push({//O .push() é usado para poder 'puxar' todos os estados da região selecionada
                 uf: estado.sigla,
                 descricao: estado.nome
             })
         }
     })
-    return aux
+    return {
+        regiao: reg,
+        estados: aux
+    }
+}
+
+const getCapitalPais = function (uf) {
+    let capitais = []
+
+    listaDeEstados.estados.forEach(function(capitalBr) {
+        if (capitalBr.capital_atual === uf) {
+            capitais.push({
+                uf: capitalBr.sigla,
+                descricao: capitalBr.nome,
+                capital: capitalBr.capital,
+                regiao: capitalBr.regiao,
+                capital_pais_ano_inicio: capitalBr.ano_inicio,
+                capital_pais_ano_termino: capitalBr.false,
+                capital_atual: capitalBr.capital_atual
+            })
+        }
+    })
+    return capitais
 }
 
 //console.log(getDadosEstado('MT'))
 //console.log(getCapitalEstado('RN'))
-console.log(getEstadosRegiao('Nordeste'))
+// console.log(getEstadosRegiao('Nordeste'))
+console.log(getCapitalPais())
