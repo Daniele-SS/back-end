972c7f187ff14e24df844ec250dc09808690bfd8
Daniele-SS <dev.daniele@outlook.com>
2026-04-01 08:37:24 -0300

    chore: nome dos arquivos alterados

---
+++ back-end/exercicio_06/app.js
@@ -0,0 +1,9 @@
+/************************************************************************************
+ * Objetivo: Arquivo responsável pela criação da API do projeto de Estados e Cidades
+ * Data: 01/04/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ * 
+ * Instalação do EXPRESS - npm install express --save
+ * 
+ ************************************************************************************/
