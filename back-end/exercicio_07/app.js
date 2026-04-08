0fcd2476cf4625d28e63e446ae313b0123d41fc0
Daniele-SS <dev.daniele@outlook.com>
2026-04-08 08:41:44 -0300

    feat: adiciona as permissões do cors

--- back-end/exercicio_07/app.js
+++ back-end/exercicio_07/app.js
@@ -5,3 +5,17 @@
  * Versão: 1.0
  ************************************************************************************/
 
+//Import das depedências para criar a API
+const express   = require ('express')
+const cors      = require ('cors')
+
+const app = express() //Criando um objeto para manipular o express
+
+//Conjunto de permissões a serem aplicadas no cors da API
+const corsOptions = {
+    origin: ['*'], 
+    methods: 'GET', 
+    allowedHeaders: ['Content-type', 'Autorization'] //São permissões de cabeçalho do cors
+}
+
+app.use(cors(corsOptions)) //Configura as permissões da API através do cors
