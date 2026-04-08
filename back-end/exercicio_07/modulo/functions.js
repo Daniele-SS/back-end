050b0977792be4d271ecf3bcdafb6aa63ec8ad9b
Daniele-SS <dev.daniele@outlook.com>
2026-04-08 08:49:32 -0300

    feat: adiciona arquivo das funções

---
+++ back-end/exercicio_07/modulo/functions.js
@@ -0,0 +1,8 @@
+/**************************************************************
+ * Objetivo: Obter uma lista de contatos para a criação da API
+ * Autora: Daniele Silva Santos
+ * Data: 08/04/2026
+ * Versão: 1.0
+ **************************************************************/
+
+const contatos = require ('./contatos.js')
