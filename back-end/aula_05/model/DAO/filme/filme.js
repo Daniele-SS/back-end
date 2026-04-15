36d311b2e7922651ab564c8400202c8119bb261f
Daniele-SS <dev.daniele@outlook.com>
2026-04-15 09:58:04 -0300

    feat: iniciado o arquivo responsável pelo CRUD no BD

---
+++ back-end/aula_05/model/DAO/filme/filme.js
@@ -0,0 +1,6 @@
+/**********************************************************************************
+ * Obejtivo: Arquivo responsável pelo CRUD no Banco de Dados MySQL na tabela Filme
+ * Data: 15/04/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ **********************************************************************************/
