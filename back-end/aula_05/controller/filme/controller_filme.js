7ed87b86d5693a5dd5f8b1421f2164e2887c4fe6
Daniele-SS <dev.daniele@outlook.com>
2026-04-17 10:36:29 -0300

    feat: adiciona o import do arquivo DAO para fazer o CRUD do filme no banco de dados e os returns das validações

--- back-end/aula_05/controller/filme/controller_filme.js
+++ back-end/aula_05/controller/filme/controller_filme.js
@@ -9,21 +9,45 @@
 //Import do arquivo de padronização de mensagens
 const config_message = require('../modulo/configMessages.js')
 
+//Import do arquivo DAO para fazer o CRUD do filme no banco de dados
+const filmeDAO = require('../../model/DAO/filme/filme.js')
+
 //Função para inserir um novo filme
 const inserirNovoFilme = async function(filme) {
+
+    let message = JSON.parse(JSON.stringify(config_message)) /*Criando um clone do objeto JSON para manipular 
+                                                            a sua estrutura local sem modificar a estrutura original*/
+
+
+    //Validação de dados para os atributos do Filme (Status 400)
     if(filme.nome == '' || filme.nome == null || filme.nome == undefined || filme.nome.length > 80) {
+        message.ERROR_BAD_REQUEST.field = '[NOME] INVÁLIDO'
+        return message.ERROR_BAD_REQUEST //400
 
     } else if(filme.data_lancamento == '' || filme.data_lancamento == null || filme.data_lancamento == undefined || filme.data_lancamento != 10) {
+        message.ERROR_BAD_REQUEST.field = '[DATA_LANÇAMENTO] INVÁLIDO'
+        
 
     } else if(filme.duracao == '' || filme.duracao == null || filme.duracao == undefined || filme.duracao < 5) {
+        message.ERROR_BAD_REQUEST.field = '[DURAÇÃO] INVÁLIDO'
+        
 
     } else if(filme.sinopse == '' || filme.sinopse == null || filme.sinopse == undefined) {
+        message.ERROR_BAD_REQUEST.field = '[SINOPSE] INVÁLIDO'
+        
 
     } else if(isNaN(filme.avaliacao) || filme.avaliacao.length > 3) {
+        message.ERROR_BAD_REQUEST.field = '[AVALIAÇÃO] INVÁLIDO'
+        
 
     } else if(filme.valor == '' || filme.valor == null || filme.valor == undefined || filme.valor.length > 5 || isNaN(filme.valor)) {
+        message.ERROR_BAD_REQUEST.field = '[VALOR] INVÁLIDO'
+
 
     } else if(filme.capa.length > 255) {
+        message.ERROR_BAD_REQUEST.field = '[CAPA] INVÁLIDO'
+
+    } else {
 
     }
 
