aac1b32b13ad84a439cdf029df15c8ebe2a907cc
Daniele-SS <dev.daniele@outlook.com>
2026-04-17 08:17:57 -0300

    feat: adiciona as funções no arquivo da controller

---
+++ back-end/aula_05/controller/filme/controller_filme.js
@@ -0,0 +1,36 @@
+/**************************************************************
+ * Objetivo: Arquivo responsável pela validação, tratamento e 
+ *              manipulação de dados para o CRUD de filmes
+ * Data: 17/04/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ **************************************************************/
+
+//Função para inserir um novo filme
+const inserirNovoFilme = async function() {
+    
+}
+
+
+//Função para atualizar um filme
+const atualizarFilme = async function() {
+    
+}
+
+
+//Fução para retornar todos os filmes
+const listarFilme = async function() {
+
+}
+
+
+//Função para buscar um filme pelo ID
+const buscarFilme = async function() {
+
+}
+
+
+//Função para excluir um filme
+const excluirFilme = async function() {
+    
+}
