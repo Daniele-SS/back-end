80d857c784d435471d082ac51c83897f6e4a9530
Daniele-SS <dev.daniele@outlook.com>
2026-04-17 10:11:06 -0300

    feat: adiciona informações do arquivo responsável pela configuração e padronização das mensagens da API

---
+++ back-end/aula_05/controller/modulo/configMessages.js
@@ -0,0 +1,28 @@
+/**************************************************************************************
+ * Objetivo: Arquivo responsável pela configuração e padronização das mensagens da API
+ * Data: 17/04/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ **************************************************************************************/
+
+//Padronização de cabeçalho para retorno dos endpoint da API
+const defaultMessage = {
+    api_description: 'API para gerenciar o controle de filmes',
+    development: 'Daniele Silva Santos',
+    version: '1.0.4.26',
+    status: Boolean, 
+    status_code: Number,
+    response: {}
+}
+
+//Mensagens de erro da API
+const ERROR_BAD_REQUEST = {
+    status: false, 
+    status_code: 400, 
+    message: 'Os dados enviados na requisição não estão corretos.'
+}
+
+module.exports = {
+    defaultMessage,
+    ERROR_BAD_REQUEST
+}
