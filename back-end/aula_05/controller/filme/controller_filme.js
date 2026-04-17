d87974f99d882235e206d7475daf9a2186575636
Daniele-SS <dev.daniele@outlook.com>
2026-04-17 09:10:30 -0300

    feat: adiciona as validações da entrada de dados que irá chegar na API

--- back-end/aula_05/controller/filme/controller_filme.js
+++ back-end/aula_05/controller/filme/controller_filme.js
@@ -7,8 +7,23 @@
  **************************************************************/
 
 //Função para inserir um novo filme
-const inserirNovoFilme = async function() {
-    
+const inserirNovoFilme = async function(filme) {
+    if(filme.nome == '' || filme.nome == null || filme.nome == undefined || filme.nome.length > 80) {
+
+    } else if(filme.data_lancamento == '' || filme.data_lancamento == null || filme.data_lancamento == undefined || filme.data_lancamento != 10) {
+
+    } else if(filme.duracao == '' || filme.duracao == null || filme.duracao == undefined || filme.duracao < 5) {
+
+    } else if(filme.sinopse == '' || filme.sinopse == null || filme.sinopse == undefined) {
+
+    } else if(isNaN(filme.avaliacao) || filme.avaliacao.length > 3) {
+
+    } else if(filme.valor == '' || filme.valor == null || filme.valor == undefined || filme.valor.length > 5 || isNaN(filme.valor)) {
+
+    } else if(filme.capa.length > 255) {
+
+    }
+
 }
 
 
@@ -32,5 +47,5 @@ const buscarFilme = async function() {
 
 //Função para excluir um filme
 const excluirFilme = async function() {
-    
+
 }
