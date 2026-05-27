1c248eddf93c6dcd5a2a2b3a756edd14a4b84a02
Daniele-SS <dev.daniele@outlook.com>
2026-05-27 10:36:31 -0300

    feat: retorna a configuração de mensagens

--- back-end/aula_05/controller/modulo/configMessages.js
+++ back-end/aula_05/controller/modulo/configMessages.js
@@ -1,54 +1,86 @@
-/*******************************************************************************
- * Objetivo: Arquivo responsável pela configuração e padronização das mensagens
- *      da API
+/**************************************************************************************
+ * Objetivo: Arquivo responsável pela configuração e padronização das mensagens da API
  * Data: 17/04/2026
- * Autor: Marcel
- * Versão: 1.0
- ******************************************************************************/
+ * Autora: Daniele Silva Santos
+ * **************************************************************************************/
 
 //Padronização de cabeçalho para retorno dos endpoint da API
 const DEFAULT_MESSAGE = {
-    api_description: 'API para gerenciar o controle de Filmes',
-    development: 'Marcel Neves Teixeira',
+    api_description: 'API para gerenciar o controle de filmes',
+    development: 'Daniele Silva Santos',
     version: '1.0.4.26',
+    status: Boolean, 
     status: Boolean,
     status_code: Number,
     response: {}
 }
 
-//Mensagens de erro da API
-const ERROR_BAD_REQUEST                 = {status: false, status_code: 400, message: 'Os dados enviados na requisição não estão corretos.'}
-const ERROR_INTERNAL_SERVER_MODEL       = {status: false, status_code: 500, message: 'Não foi possível processar a requisição por conta de erro na API [ERRO NA MODELAGEM DE DADOS].'}
-const ERROR_INTERNAL_SERVER_CONTROLLER  = {status: false, status_code: 500, message: 'Não foi possível processar a requisição por conta de erro na API [ERRO NA CONTROLLER].'}
-const ERROR_CONTENT_TYPE                = {status: false, status_code: 415, message: 'Não foi possível processar a requisição, pois o formato de dados aceito pela API é somente JSON.'}
-const ERROR_NOT_FOUND                   = {status: false, status_code: 404, message: 'Não foi encontrado nenhum dado para retorno.'}
 
-//Mensagens de Sucesso da API
-const SUCCESS_CREATED_ITEM  =   {status: true, status_code: 201, message: 'Registro inserido com sucesso!'}
+//Mensagens de erro da API 
+const ERROR_BAD_REQUEST = {
+    status: false, 
+    status_code: 400, 
+    message: 'Os dados enviados na requisição não estão corretos'
+}
 
-//Mensagens de Sucesso da API
-const SUCCESS_CREATED_ITEM_WARNIG  =   {status: true, status_code: 201, message: 'Os dados principais foram inseridos com sucesso, porém alguns apresentaram problemas!'}
+const ERROR_CONTENT_TYPE = {
+    status: false, 
+    status_code: 415, 
+    message: 'Não foi possível processar a requisição, pois o tipo de dados aceito pela API é somente JSON'
+}
 
-//Retornos para GET 200
-const SUCCESS_RESPONSE      =   {status: true, status_code: 200}
+const ERROR_NOT_FOUND = {
+    status: false, 
+    status_code: 404, 
+    message: 'Não foi encontrado nenhum dado para retorno'
+}
 
-//Retornos para PUT 200
-const SUCCESS_UPDATED_ITEM  =   {status: true, status_code: 200, message: 'Registro atualizado com sucesso!'}  
+const ERROR_INTERNAL_SERVER_MODEL = {
+    status: false, 
+    status_code: 500, 
+    message: 'Não foi possível processar a requisição por conta de erro na API [ERRO NA MODELAGEM DE DADOS]'
+}
 
-//Retorno para DELETE (200 ou 204)
-const SUCCESS_DELETED_ITEM =    {status: true, status_code: 200, message: 'Registro excluído com sucesso!'}
+const ERROR_INTERNAL_SERVER_CONTROLLER = {
+    status: false, 
+    status_code: 500, 
+    message: 'Não foi possível processar a requisição por conta de erro na API [ERRO NA CONTROLLER]'
+}
 
+
+//Mensagens de sucesso da API
+const SUCCESS_CREATED_ITEM = {
+    status: true,
+    status_code: 201,
+    message: 'Registro inserido com sucesso'
+}
+
+const SUCCESS_RESPONSE = {
+    status: true,
+    status_code: 200, //GET
+}
+
+const SUCCESS_UPDATE_ITEM = {
+    status: true,
+    status_code: 200, //PUT
+    message: 'Registro atualizado com sucesso'
+}
+
+const SUCCESS_DELETED_ITEM = {
+    status: true,
+    status_code: 200, //O status code 204 (No Content) também poderia ser usado
+    message: 'Registro deletado com sucesso'
+}
 
 module.exports = {
     DEFAULT_MESSAGE,
     ERROR_BAD_REQUEST,
-    ERROR_INTERNAL_SERVER_MODEL,
-    ERROR_INTERNAL_SERVER_CONTROLLER,
-    ERROR_CONTENT_TYPE,
-    ERROR_NOT_FOUND,
     SUCCESS_CREATED_ITEM,
+    ERROR_INTERNAL_SERVER_MODEL,
+    ERROR_CONTENT_TYPE,
+    ERROR_INTERNAL_SERVER_CONTROLLER,
+    ERROR_NOT_FOUND,
     SUCCESS_RESPONSE,
-    SUCCESS_UPDATED_ITEM,
-    SUCCESS_DELETED_ITEM,
-    SUCCESS_CREATED_ITEM_WARNIG
+    SUCCESS_UPDATE_ITEM,
+    SUCCESS_DELETED_ITEM
 }
